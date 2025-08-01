b7170ede39b43acaec88570cac09691de90a31bd
DevvysEngines <maskertacker@gmail.com>
2025-07-31 18:51:26 -0700

    Delete v9/v9 directory

--- v9/v9/v9.js
+++
@@ -1,9 +0,0 @@
-import { module } from "./module/module.js";
-import { utilities } from "./utilities.js";
-import { script } from "./script.js";
-
-export class v9 {
-    static module = module;
-    static utilities = utilities;
-    static script = script;
-}
